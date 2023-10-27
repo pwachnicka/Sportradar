f08cede51432d6f3dff392add654992e00fee3b4
pwachnicka <pwachnicka@sii.pl>
2023-10-27 12:49:05 +0200

    Adding source data

---
+++ sourceData.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+    [
+        'teams' => [
+            'home' => 'Mexico',
+            'away' => 'Canada'
+        ],
+        'score' => [
+            [
+                'home' => 0,
+                'away' => 1,
+            ],
+            [
+                'home' => 0,
+                'away' => 2,
+            ],
+            [
+                'home' => 0,
+                'away' => 3,
+            ],
+            [
+                'home' => 0,
+                'away' => 4,
+            ],
+            [
+                'home' => 0,
+                'away' => 5,
+            ]
+        ]
+    ],
+    [
+        'teams' => [
+            'home' => 'Spain',
+            'away' => 'Brazil'
+        ],
+        'score' => [
+            [
+                'home' => 1,
+                'away' => 0,
+            ],
+            [
+                'home' => 2,
+                'away' => 0,
+            ],
+            [
+                'home' => 2,
+                'away' => 1,
+            ],
+            [
+                'home' => 3,
+                'away' => 1,
+            ],
+            [
+                'home' => 4,
+                'away' => 1,
+            ],
+            [
+                'home' => 5,
+                'away' => 1,
+            ],
+            [
+                'home' => 6,
+                'away' => 2,
+            ],
+            [
+                'home' => 7,
+                'away' => 2,
+            ],
+            [
+                'home' => 8,
+                'away' => 2,
+            ],
+            [
+                'home' => 9,
+                'away' => 2,
+            ],
+            [
+                'home' => 10,
+                'away' => 2,
+            ]
+        ],
+    ]
+];
