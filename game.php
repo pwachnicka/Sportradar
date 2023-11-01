822c224233cee3583685ed1717d890a591dbb6ad
pwachnicka <pwachnicka@sii.pl>
2023-11-01 22:39:03 +0100

    Main game file

--- game.php
+++ game.php
@@ -1,5 +1,30 @@
 <?php
-
 require __DIR__ . '/vendor/autoload.php';
 
-// initial Live Football World Cup
+use Sportradar\MatchResults;
+use Sportradar\ScoreBoard\ScoreBoard;
+use Sportradar\Sorting\ReverseOrderWithLIFOComparator;
+use Sportradar\Team\AwayTeam;
+use Sportradar\Team\HomeTeam;
+
+$sourceData = require_once('data/sourceData.php');
+
+$matchResults = new MatchResults();
+$scoreBoard = new ScoreBoard();
+
+foreach ($sourceData as $match) {
+
+    $homeTeam = new HomeTeam($match['teams']['home']);
+    $awayTeam = new AwayTeam($match['teams']['away']);
+
+    $scoreBoard->start($homeTeam, $awayTeam);
+
+    foreach ($match['score'] as $score) {
+        $scoreBoard->update($score['home'], $score['away']);
+    }
+
+    $scoreBoard->finish();
+    $matchResults->save($homeTeam, $awayTeam);
+}
+ReverseOrderWithLIFOComparator::sortResults($matchResults);
+print_r($matchResults->getSummary());
